26a21e16888c06133cbf7f7c7baf87ed851236be
knel2 <knel2@uw.edu>
2026-03-01 14:00:50 -0800

    made a few changes in my code and looked a the datsets to make map

--- js/map3.js
+++ js/map3.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken =
-'token';
+'pk.eyJ1Ijoia25lbDIiLCJhIjoiY21rdTlkYmx5MThyZjNmcHVrMDYzdXJ4dyJ9.HrXpvWOlXxo7fGCGnGQG6A';
 
 const map = new mapboxgl.Map({
     container: 'map',
@@ -8,4 +8,23 @@ const map = new mapboxgl.Map({
     center: [-122.335167, 47.608013], // Seattle
     zoom: 11
 });
+
+// map.addSource('carownership', {
+//     type: 'geojson',
+//     data: 'assets/KingCounty_Vehicle_Access_Clean.json'
+// })
+
+// map.addLayer({
+//     'id': 'carownc'
+//     'type' : 'fill',
+//     'source': 'carownership',
+//     'paint' {
+//         'fill-color': [
+//             'step',
+//             ['get', 'density'],
+
+//         ]
+//     }
+
+// })
 
